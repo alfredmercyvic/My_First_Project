8c1416b5011d9e99e33046fe5d9a92830eccd5c8
alfredmercyvic <mercyvicalfred20@gmail.com>
2024-10-02 18:02:35 -0700

    First Commit

---
+++ Lesson_4.py
@@ -0,0 +1,15 @@
+def mercyvic_alfred_function():
+    # snake case
+    my_name = "Alfred Mercyvic"
+    # pascal case
+    MyAge = 12
+    # camel case
+    isFemale = True
+    # snake and pascal case
+    My_Height = 5.8
+    print("My name is " + my_name)
+    print("I am " , MyAge , " years old")
+    print("Am i a female? " , isFemale)
+    print("I am " , My_Height , " tall")
+
+mercyvic_alfred_function()
