ec3411df2f7fcd0ba1d7e9826576085a135adc7c
alfredmercyvic <mercyvicalfred20@gmail.com>
2024-10-04 00:43:47 -0700

    Lesson_5

---
+++ Lesson_5.py
@@ -0,0 +1,32 @@
+# A static function to multiply three numbers
+def multiply_num():
+    num1 = 2
+    num2 = 3
+    num3 = 4
+    print(num1 * num2 * num3)
+multiply_num()    
+
+# A dynamic function to multiply three numbers
+def multiply_num(num1,num2,num3):
+    print(num1 * num2 * num3)
+multiply_num(2,3,4)   
+
+# Observation
+# The product(2*3*4) equals 24, indicates that multiplication grows the result significantly compared to addition.
+
+
+# A static function to add three numbers
+def add_three_numbers():
+    A = 12
+    B = 5
+    C = 3
+    print(A + B + C)
+add_three_numbers()  
+
+# A dynamic function to add three numbers
+def add_three_numbers(A, B, C):
+    print(A + B + C)
+add_three_numbers(12, 5, 3)
+
+# Observation
+# The sum(12,5,3) equals 20, which is the combined value of the hree variables.
